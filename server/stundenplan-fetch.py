66145109c35c1de5653cfa42e08c0bb1b19849a0
Carl van Vliet <100851058+carlvv@users.noreply.github.com>
2025-11-19 16:23:50 +0100

    Update docker-compose.yml, Dockerfile, requirements.txt, and 3 more files

--- server/stundenplan-fetch.py
+++ server/stundenplan-fetch.py
@@ -1,13 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
 options = webdriver.FirefoxOptions()
 options.add_argument('--headless')
 driver = webdriver.Firefox(options=options)
-
 
 url = "https://intern.fh-wedel.de/~splan/index.html?typ=benutzer_vz"
 driver.get(url)
@@ -19,7 +17,7 @@ for checkbox in checkboxes:
     if not checkbox.is_selected():
         checkbox.click()
 
-submit_button = driver.find_element(By.CSS_SELECTOR, "input[type='submit', value='Ergebnis erstellen']")
+submit_button = driver.find_element(By.CSS_SELECTOR, "input[type='submit'][value='Ergebnis erstellen']")
 submit_button.click()
 
 time.sleep(5)
