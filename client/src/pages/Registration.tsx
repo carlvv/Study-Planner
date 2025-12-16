0b43490417bd36c5f85951efa8ef22e8717623ab
Marty <stud106282@fh-wedel.de>
2025-12-16 18:11:58 +0100

    Frontend: Registrierung - Fehlermeldung hinzugefügt

--- client/src/pages/Registration.tsx
+++ client/src/pages/Registration.tsx
@@ -3,8 +3,8 @@ import { type Student, type Curricula } from "../types";
 import { ButtonPrimary, IconButton, IconLink } from "../componenten/Buttons";
 import { program_version, program_name } from "../data/Curricula";
 import { ArrowLeft } from "lucide-react";
-//import type { Student } from "../types";
 
+//TODO: richtiges salt holen
 const PASSWORD_SALT: string = "salt";
 
 type FormType = {
@@ -16,13 +16,26 @@ type FormType = {
 };
 
 export default function Registration() {
+  //Speichert auf welcher Seite sich der User befindet
   const [step, setStep] = useState(1);
+
   const [curriculaData, setCurriculaData] = useState<Curricula>({
     isBachelor: true,
     program_name: "",
     program_version: "",
   });
+
+  //Speichert die Eingaben vom Formular
   const [formData, setFormData] = useState<FormType>({
+    student_id: "",
+    name: "",
+    start_semester: "",
+    password: "",
+    password_confirm: "",
+  });
+
+  //Zum setzen der Fehlermeldung für jedes Eingabefeld
+  const [formErrors, setFormErrors] = useState<FormType>({
     student_id: "",
     name: "",
     start_semester: "",
@@ -40,14 +53,15 @@ export default function Registration() {
   //TODO: CSS
   return (
     <div>
+      {/* "Zurück-Pfeil"*/}
       {step === 1 && <IconLink to={"/"} Icon={ArrowLeft}></IconLink>}
       {step > 1 && (
         <IconButton Icon={ArrowLeft} onClick={() => prevStep()}></IconButton>
       )}
-
+      {/* Erste Seite - Studienrichtung*/}
       {step === 1 && (
         <div>
-          <h1>Was studieren Sie?</h1>
+          <label>Was studieren Sie?</label>
           <ButtonPrimary
             onClick={() => {
               setCurriculaData({ ...curriculaData, isBachelor: true });
@@ -68,12 +82,13 @@ export default function Registration() {
         </div>
       )}
 
+      {/* Zweite Seite - Studiengang*/}
       {step === 2 && (
         <div>
-          <h1>
+          <label>
             Welchen {curriculaData.isBachelor ? "Bachelor" : "Master"}{" "}
             Studiengang?
-          </h1>
+          </label>
           {localProgram_name.map((name, index) => (
             <ButtonPrimary
               key={index}
@@ -88,9 +103,10 @@ export default function Registration() {
         </div>
       )}
 
+      {/* Dritte Seite - Studienordnung*/}
       {step === 3 && (
         <div>
-          <h1>Wählen Sie ihre Studienordnung aus</h1>
+          <label>Wählen Sie ihre Studienordnung aus</label>
           {localProgram_version.map((version, index) => (
             <ButtonPrimary
               key={index}
@@ -108,10 +124,12 @@ export default function Registration() {
         </div>
       )}
 
+      {/* Vierte Seite - Formular für Benutzerdaten*/}
       {step === 4 && (
         <div>
           <h1>Bitte füllen Sie das Formular aus.</h1>
-          <h2>Dein vollständiger Name:</h2>
+          <label>Dein vollständiger Name:</label>
+          {formErrors.name !== "" && <p className="error">{formErrors.name}</p>}
           <input
             type="text"
             name="name"
@@ -120,7 +138,10 @@ export default function Registration() {
               setFormData({ ...formData, name: e.target.value });
             }}
           />
-          <h2>Deine Matrikelnummer:</h2>
+          <label>Deine Matrikelnummer:</label>
+          {formErrors.student_id !== "" && (
+            <p className="error">{formErrors.student_id}</p>
+          )}
           <input
             type="text"
             name="student_id"
@@ -129,7 +150,10 @@ export default function Registration() {
               setFormData({ ...formData, student_id: e.target.value });
             }}
           />
-          <h2>Dein Start-Semester:</h2>
+          <label>Dein Start-Semester:</label>
+          {formErrors.start_semester !== "" && (
+            <p className="error">{formErrors.start_semester}</p>
+          )}
           <input
             type="text"
             name="start_semester"
@@ -141,7 +165,10 @@ export default function Registration() {
               });
             }}
           />
-          <h2>Dein Passwort:</h2>
+          <label>Dein Passwort:</label>
+          {formErrors.password !== "" && (
+            <p className="error">{formErrors.password}</p>
+          )}
           <input
             type="password"
             name="password"
@@ -149,7 +176,10 @@ export default function Registration() {
               setFormData({ ...formData, password: e.target.value });
             }}
           />
-          <h2>Passwort bestätigen:</h2>
+          <label>Passwort bestätigen:</label>
+          {formErrors.password_confirm !== "" && (
+            <p className="error">{formErrors.password_confirm}</p>
+          )}
           <input
             type="password"
             name="password_confirm"
@@ -159,7 +189,17 @@ export default function Registration() {
           />
           <ButtonPrimary
             to={"/"}
-            onClick={() => sendData(curriculaData, formData)}
+            onClick={() => {
+              const formErrors: FormType = validateForm(formData);
+              setFormErrors(formErrors);
+              //keine Fehler?
+              const noErrors = Object.values(formErrors).every(
+                (value) => value === ""
+              );
+              if (noErrors) {
+                sendData(curriculaData, formData);
+              }
+            }}
           >
             Absenden
           </ButtonPrimary>
@@ -199,41 +239,6 @@ function calcPasswordHash(password: string, salt: string): string {
  * @param formData Daten über den Studenten
  */
 function sendData(curriculaData: Curricula, formData: FormType): void {
-  //Nicht alles ausgefüllt?
-  const { start_semester, ...requiredFields } = formData;
-  const allFilled = Object.values(requiredFields).every((v) => v.trim() !== "");
-
-  if (!allFilled) {
-    alert("Bitte füll alle Pflichtfelder aus");
-    return;
-  }
-
-  //Matrikelnummer gültig?
-  const num = Number(formData.student_id);
-
-  if (isNaN(num) || formData.student_id.length > 6) {
-    alert("Ungültige Matrikelnummer");
-    return;
-  }
-
-  //TODO: Start-Semester Validierung
-  //löschen wenn start_semester benutzt wurde
-  if (start_semester === start_semester) {
-    console.log();
-  }
-
-  if (formData.password.length < 8) {
-    //Passwort zu kurz?
-    alert("Passwort zu kurz");
-    return;
-  }
-
-  //Passwort-Bestätigung richtig?
-  if (formData.password !== formData.password_confirm) {
-    alert("Die Passwort-Bestätigung war nicht korrekt");
-    return;
-  }
-
   const studentData: Student = {
     student_id: formData.student_id,
     name: formData.name,
@@ -245,4 +250,43 @@ function sendData(curriculaData: Curricula, formData: FormType): void {
 
   //TODO: Daten speichern
   console.log(studentData);
+}
+
+/**
+ * Validiert die Benutzereingaben im Formular und gibt ein Objekt zurück, dass die Fehlermeldungen für jedes Eingabefeld enthält.
+ *
+ * @param formData Daten des Formulars
+ * @returns Objekt, dass die Fehlermeldungen für jedes Eingabefeld enthält
+ */
+function validateForm(formData: FormType): FormType {
+  //Alle Felder ausgefüllt?
+  const formErrors: FormType = Object.fromEntries(
+    (Object.keys(formData) as Array<keyof FormType>).map((key) => [
+      key,
+      formData[key] === "" ? "Fülle das Feld aus" : "",
+    ])
+  ) as FormType;
+  //Start_Semester darf leer sein da optional
+  formErrors.start_semester = "";
+
+  //Matrikelnummer gültig?
+  const num = Number(formData.student_id);
+
+  if (isNaN(num) || formData.student_id.length > 6) {
+    formErrors.student_id = "Ungültige Matrikelnummer";
+  }
+
+  //TODO: Start-Semester Validierung
+
+  //Passwort zu kurz?
+  if (formData.password.length < 8) {
+    formErrors.password = "Passwort zu kurz";
+  }
+
+  //Passwort-Bestätigung richtig?
+  if (formData.password !== formData.password_confirm) {
+    formErrors.password_confirm = "Die Passwort-Bestätigung ist nicht korrekt";
+  }
+
+  return formErrors;
 }
